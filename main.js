77a2c495ac39011b9869e70a5985fad98cc0e090
Benaiah Kilen <benaiahkilen@gmail.com>
2022-12-03 13:28:42 -0600

    Quick documentation & fixes

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ client.on("ready", async () => {
     Logger.success(`[${client.guilds.cache.size} Guilds] - [${client.channels.cache.size} Channels] - [${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()} Users]`)
 
     /* Initialize UserDB if not found */
+    // TODO: Move this into two separate methods (one to check, one to initialize)
+    // TODO: Create UserDB.db file if not found initially
     SQLiteUtil.IsUserDBInitialized(client)
 
     /* REGISTER SLASH COMMANDS */
@@ -33,12 +35,13 @@ client.on("ready", async () => {
         console.error(error)
     }
 
-    /* Turn on Daily Message */
+    /* DAILY MESSAGE */
+    // TODO: Move to own service or util
     cron.schedule("0 8 * * * *", function() {
         SQLiteUtil.Select(
             "SELECT M.MESSAGE_CONTENT MESSAGE_CONTENT, U.USER_NAME USER_NAME FROM MESSAGE M INNER JOIN USERGUILDMESSAGE UGM ON UGM.MESSAGE_ID = M.MESSAGE_ID INNER JOIN USER U ON U.USER_ID = UGM.USER_ID ORDER BY RANDOM()", []).then((result) => {
             if (result && result.MESSAGE_CONTENT && result.USER_NAME) {
-                Logger.error(`**${result.USER_NAME}:** ${result.MESSAGE_CONTENT}`)
+                Logger.info(`**${result.USER_NAME}:** ${result.MESSAGE_CONTENT}`)
             } else {
                 Logger.error("Unable to grab random message.")
             }
@@ -52,12 +55,15 @@ client.on("interactionCreate", async interaction => {
     const command = client.commands.get(interaction.commandName)
     if (!command) return
 
+    /* Make sure interaction user is found in GUILD & USER tables */
     const GuildUserIds = await SQLiteUtil.ValidateGuildAndUser(interaction.guild, interaction.user)
     command.execute(interaction, client)
 })
 
 client.on('messageCreate', async message => {
     if (message.author.bot) return
+
+    /* Make sure message author is found in GUILD & USER tables */
     const GuildUserIds = await SQLiteUtil.ValidateGuildAndUser(message.guild, message.author)
     MessageUtil.LogMessage(true, message.guild, message.author, message)
 })
